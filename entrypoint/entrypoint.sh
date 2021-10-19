20eb9558c46e95c2b84a2bcc4cf8a0c5526f581c
rsteube <rsteube@users.noreply.github.com>
2021-10-20 01:30:41 +0200

    tmp

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -2,4 +2,7 @@
 
 cat /jsonresume-theme-aurora/resume.json | jq --from-file /entrypoint/vcard.filter --raw-output | qrencode --type SVG > /jsonresume-theme-aurora/public/qrcode.svg
 
+start=$(cat /jsonresume-theme-aurora/resume.json | jq --raw-output '.work[].startDate' | sort | head -1)
+sed -i "s/Total Experience: {{totalWorkExp '.*'}}/Total Experience: {{totalWorkExp '${start}'}}/" /jsonresume-theme-aurora/src/components/work-experience.hbs
+
 exec "$@"
